3fe050ba491761690e9bc0fe17c9eee1e28ae43f
benjamin ifejika <benjaminifejika393@gmail.com>
2022-09-10 22:59:28 -0700

    just great

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/*the outcome is a piece of art*/
+#include <stdio.h>
+/**
+ * main-this is where our code begins
+ * @void-this shows that our main has no arguments
+ * Return:zero
+ */
+int main(void)
+{
+
+	/*
+	 * Display a message
+	 */
+	printf(with proper grammar, but the outcome is a piece of art);
+	return (0);
+}
