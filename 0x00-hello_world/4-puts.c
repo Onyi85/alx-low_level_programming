e6f03144f2d562b21acfb4eb836c78e2af08db74
benjamin ifejika <benjaminifejika393@gmail.com>
2022-09-10 22:08:33 -0700

    i never give up

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/*this is about building a multilingual puzzle*/
+#include <stdio.h>
+/**
+ * main- The is where our code begins
+ * @void- this shows that our main has no arguments
+ * Return: zero
+ */
+int main(void)
+{
+	/*
+	 * Display a message
+	 */
+	puts("\"Programming is like building a multilingual puzzle/n");
+	return (0);
+
+}
