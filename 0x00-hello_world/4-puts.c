dbf33a269a08a0d0315bdd8e34e1a0785efa0eca
benjamin ifejika <benjaminifejika393@gmail.com>
2022-09-10 22:26:08 -0700

    i never give up

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,6 +10,6 @@ int main(void)
 	/*
 	 * Display a message
 	 */
-	puts("\"Programming is like building a multilingual puzzle/n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
