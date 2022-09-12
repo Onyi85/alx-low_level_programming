2df84410b6c433368cfa8b82231aad307c80c509
benjamin ifejika <benjaminifejika393@gmail.com>
2022-09-12 13:29:58 -0700

    alx

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,21 +14,34 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	/*
+	*set the last digit for n to k
+	*/
 	k = n % 10;
+	/*
+	 * check if k is bigger than 5
+	 */
 	if (k > 5)
 		/*
-		*Display this message if the condition is true
-		*/
+		 * if true: display this message
+		 */
 		printf("last digit of %d is %d and is greater than 5\n", n, k);
+	/*
+	 * check if k is 0
+	 */
 	else if (k == 0)
 		/*
 		*Display if true
 		*/
 		printf("last digit of %d is %d and is 0\n", n, k);
+	/*
+	 * otherwise it is something else
+	 */
 	else
-		printf("last digit of %d is %d and is less than 6 and not 0\n", n, k);
 		/*
-		*Display if true
-		*
+		* if true: display this message
+		*/
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, k);
+
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+/* its about lowercase followed by a newline*/
+#include <stdio.h>
+/**
+ * main-this is where our code bigins
+ * @void-this shows that our main has no arguments
+ * Return:zero
+ */
+int main(void)
+int (_c)
+'_attribute_'
+{
+	
+	/*
+	 * char ch;
+	 */
+	putchar ("|char* = a; ch; c; = z"; "char++";)
+		/*
+		*if the condition is true
+		*char=lower(ch);
+		*putchar(ch);
+		*/
+	putchar{'\n'};
+	return (0);
+
+
+}
