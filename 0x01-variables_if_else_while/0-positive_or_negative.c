bd112d3a2624c6b09d5c65676718e4b6e5a1f701
benjamin ifejika <benjaminifejika393@gmail.com>
2022-09-11 18:12:40 -0700

    alx

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+/*this is about possitive and nagative*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/*main-this is where our code begins*/
+/*@void-this shows that our main has no arguments*/
+/*betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/*check if n is bigger than zero*/
+     if (n > 0)
+        /*
+	 * print the message if condition is true
+	 */
+		printf("%d is positive\n",n);
+        /*check if n is zero
+	 */
+     else if (n == 0)
+	/*
+	 * prints if codition is true
+	 */
+		printf("%d is zero\n",n);
+     /*
+     *check if n is zero
+     */
+     else
+		printf("%d is negative\n",n);
+     return (0);
+}
