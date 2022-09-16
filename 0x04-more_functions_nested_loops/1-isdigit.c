56c47d70289efecb21d6679f202154ced04d02f6
benjamin ifejika <benjaminifejika393@gmail.com>
2022-09-15 19:46:18 -0700

    ALL

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+/**
+ * _isdigit-determine if digit
+ *
+ * @c:character to determine
+ *
+ * Return: gives boolean
+ */
+int_isdigit(int c)
+{
+	if (c >= 'o' && c <= '9')
+		return (1);
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,7 +5,7 @@ int _isupper(int c);
 int _putchar(char c);
 int mul(int a, int b);
 void print_numbers(void);
-
+int _isdigit(int c);
 
 
 
