bf033361dbfe461a1594f394d52c6ff02f0bba1e
benjamin ifejika <benjaminifejika393@gmail.com>
2022-09-19 19:38:11 -0700

    NEW FILES

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+/* this is about pointers array and strings */
+#include "main.h"
+/**
+ * swap_int a program prepared about pointers
+ * array and integer
+ * @a:a pointer tob integer
+ * @b:pointer to integer
+ */
+void swap_int(int *a, int *b)
+{
+	int val_a;
+	int val_b;
+		val_b = *b;
+		val_a = *a;
+		*a = val_b;
+		*b = val_a;
+
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 void reset_to_98(int *p);
 
 int _putchar(char c);
-
+void swap_int(int *a, int *b);
 
 
 
