88e1920c30315d3681765fcd0eca9499c988d673
benjamin ifejika <benjaminifejika393@gmail.com>
2022-09-20 17:02:10 -0700

    NEW FILES

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+/* this program print sting and reverse */
+#include "main.h"
+
+/**
+ * print_rev - desplay screen in reverse form
+ * @s: pointer to char
+ */
+void print_rev(char *s)
+{
+	int i, k, j;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	j = i - 1;
+	for (k = j; k >= 0; k--)
+		_putchar(s[k]);
+	_putchar('\n');
+}
