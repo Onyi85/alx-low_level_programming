a45dcf384e7760d0fda08699d6d91f8170d81d79
benjamin ifejika <benjaminifejika393@gmail.com>
2022-09-20 18:47:18 -0700

    ALL

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+/* this program  prints the screen in reverse */
+#include "main.h"
+/**
+ * rev_string - Display string in reverse form
+ * @s: pointer to char
+ */
+void rev_string(char *s)
+{
+	int i, l, k;
+	char *start, *end, ch;
+
+	i = 0;
+	start = s;
+	end = s;
+	while (s[i] != '\0')
+		i++;
+	l = i;
+	for (k = 0; k < l - 1; k++)
+		end++;
+	for (k = 0; k < l / 2; k++)
+	{
+		ch = *end;
+		*end = *start;
+		*start = ch;
+		start++;
+		end--;
+
+	}
+}
+
