c27627e07e712ae5bfdad2c976aa6c5728105b8f
benjamin ifejika <benjaminifejika393@gmail.com>
2022-09-21 19:23:43 -0700

    Add: a new file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+/* This program concatenate two strings */
+#include "main.h"
+/**
+* *_strcat - This function join two string
+* @dest: pointer to char
+* @src: pointer to char
+* Return: dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	int len = 0, i;
+
+	/* find the lenght */
+
+	while (dest[len])
+		len++;
+	/* for loop */
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[len] = src[i];
+		len = len + 1;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
+
