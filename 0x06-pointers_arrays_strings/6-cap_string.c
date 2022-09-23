352a612fe6156bdde5e31f66e5212c9b0310b628
benjamin ifejika <benjaminifejika393@gmail.com>
2022-09-22 19:29:54 -0700

    ALL

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,30 +11,29 @@ char *cap_string(char *s)
 	int i = 0;
 
 	/*
-	 * check first index capital
-	 */
-	if (s[i]  >= 'a' && s[i] <= 'z')
+	* check first index capital
+	*/
+	if (s[i] >= 'a' && s[i] <= 'z')
 		s[i] = s[i] - 'a' + 'A';
 	i++;
 
 	while (s[i] != '\0')
 	/*
-	 * iterate through string
-	 */
+	* iterate through string
+	*/
 	{
 	/*
-	 * if lowercase and prio char is separator, capitalize
-	 */
+	* if lowercase and prio char is separator, capitalize
+	*/
 	if ((s[i] >= 'a' && s[i] <= 'z')
 		&& (s[i - 1] == ';' || s[i - 1] == ';' || s[i - 1] == ';' ||
 		s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == ';' ||
 		s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
-		s[i - 1] == '}' || s[i - 1] == ';' || s[i - 1] == '\t'
-		|| s[i - 1] == '\n'))
+		s[i - 1] == '}' || s[i - 1] == ';' || s[i - 1] == '\t' || s[i - 1] == '\n');
 		s[i] = s[i] - 'a' + 'A';
 	i++;
 	}
 
 	return (s);
-}
 
+}
