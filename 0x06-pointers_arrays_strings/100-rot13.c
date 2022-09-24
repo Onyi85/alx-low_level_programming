0ca3f1718f885f70f2d787a5487fb8dcc8cd592b
benjamin ifejika <benjaminifejika393@gmail.com>
2022-09-23 19:45:24 -0700

    ALL

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+/* this encodes a string using rot13 */
+#include "main.h"
+/**
+* *rot13 - encodes string using rot13
+* @s: pointer to char
+* Return: s
+*/
+char *rot13(char *s)
+{
+	/*
+	 * loop counter
+	 */
+	int i;
+
+	i  = 0;
+	/*
+	 * Do this untill you fin a '\0'
+	 */
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((*(s + i) >= 'a' && *(s + i) < 'n') ||
+				(*(s + i) >= 'A' && *(s + i) <= 'M'))
+			*(s + i) += 13;
+		else if ((*(s + i) >= 'n' && *(s + i) <= 'z') ||
+				(*(s + i) >= 'N' && *(s + i) <= 'Z'))
+			*(s + i) -= 13;
+	}
+	return (s);
+}
