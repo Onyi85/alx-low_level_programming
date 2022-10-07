7e3ca22d2f973fd00d5ee070ee0e931504ec0415
benjamin ifejika <benjaminifejika393@gmail.com>
2022-10-07 09:03:05 -0700

    its all about malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+/* This program allocates memory to copy a string and returns a pointer */
+#include "main.h"
+#include <stdlib.h>
+/**
+ *  _strdup - this function allocates and copy string to memory
+ *  @str: pointer to a char
+ *  Return: a pointer to a char
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i, len, k;
+
+	if (str == NULL)
+		return (str);
+	i = 0;
+	len = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+		len++;
+	}
+
+	s = malloc((len + 1) * sizeof(char));
+	if (s == NULL)
+		return (s);
+	for (k = 0; k <= len; k++)
+		s[k] = str[k];
+	s[k] = '\0';
+	return (s);
+}
