efe5369226236c5c34d552fc159f70a30f301ad0
benjamin ifejika <benjaminifejika393@gmail.com>
2022-10-19 07:27:48 -0700

    File

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,21 +1,36 @@
 /* this program prints half of array */
 #include "main.h"
+#include <stdio.h>
 /**
- * puts_half - print half of the array
+ * puts_half - prints half of the array
  * @str: pointer to a char
  */
 void puts_half(char *str)
 {
-	int i, len;
-	char *ch;
+	int len, i, j, new_len;
 
+	len = 0;
+	j = 0;
 	i = 0;
-	ch = str;
 	while (str[i] != '\0')
+	{
+		len++;
 		i++;
-	len = i - 1;
-	for (i = 0; i < (len - 1) / 2; i++)
-		ch++;
-	for (i = 0; 1 > (len - 1) / 2; i++
-			_putchar('\n');
+	}
+	if (len % 2 == 0)
+		new_len = len / 2;
+	else
+		new_len = (len - 1) / 2;
+	while (j < new_len)
+		{
+			str++;
+			j++;
+		}
+	while (new_len < len)
+	{
+		_putchar(*str + '0');
+		str++;
+		new_len++;
+	}
+	_putchar('\n');
 }
