63577359db88a450f5671a95f58b284f7f74942a
benjamin ifejika <benjaminifejika393@gmail.com>
2022-10-19 06:49:38 -0700

    File

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,19 @@
 /* this function prints every other array of character */
-#include "main"
+#include "main.h"
+#include<stdio.h>
 /**
- * puts- print every other character of an array
+ * puts2- prints every other character of an array
  * @str: pointer to a char
  */
 void puts2(char *str)
 {
-	int i, len, j;
+	int i, len;
 
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	len = i;
-	for (j = 0; j < len - 1; j += 2)
-		_putchar(str[j]);
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	for (i = 0; i < len - 1; i += 2)
+		_putchar(*(str + i));
 	_putchar('\n');
 }
