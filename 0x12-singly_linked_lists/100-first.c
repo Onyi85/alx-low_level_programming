544258082d340e0048258730317193b78ae8ffeb
benjamin ifejika <benjaminifejika393@gmail.com>
2022-10-25 15:21:46 -0700

    Added a new file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_before_main - This is a function that is printed before main
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
