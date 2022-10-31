d81baf558e5d33073df94f88ae7ad7b302983c1e
benjamin ifejika <benjaminifejika393@gmail.com>
2022-10-31 08:51:15 -0700

    Added File

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,31 +1,36 @@
-/* this print the lenght  of a prefix substring */
-#include <stdio.h>
+/*this is the length of prefix substring*/
 #include "main.h"
 /**
-* _strspn - prints the length of a prefix substring
-* @s: pointer to char
-* @accept: pointer to char
-* Return: a char
-*/
+ *_strspn-Get the length of a prefix substring.
+ *@s:The null-terminated string to be scanned
+ *@accept:The null-terminated string containing the characters to match.
+ *
+ *Return:Returns the number of bytes in the initial segment
+ *of s which consist only of bytes from accept
+ */
+
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int count, i, m, j, k;
+	int count = 0, flag;
+	char *start = accept;
 
-
-i = 0;
-count = 0;
-k = 0;
-while (*(s + k) != ',')
-k++;
-while (*(accept + i) != '\0')
-i++;
-for (m = 0; m < k; m++)
-{
-for (j = 0; j <= i - 1; j++)
-{
-if (*(s + m) == *(accept + j))
-count++;
-}
-}
-return (count);
+	while (*s)
+	{
+		flag = 0;
+		while (*accept)
+		{
+			if (*accept == *s)
+			{
+				count++;
+				flag = 1;
+				break;
+			}
+			accept++;
+		}
+		s++;
+		accept = start;
+		if (flag == 0)
+			break;
+	}
+	return (count);
 }
